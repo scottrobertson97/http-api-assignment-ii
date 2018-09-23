30e2350f2ac6e7c9d24c40baaa802bee2cd42ffc
Scott Robertson <scottrobertson97@gmail.com>
2018-09-23 13:08:01 -0400

    eslint good

--- src/jsonResponses.js
+++ src/jsonResponses.js
@@ -4,100 +4,78 @@
 // We will be working with databases in the next few weeks.
 const users = {};
 
-//function to respond with a json object
-//takes request, response, status code and object to send
+// function to respond with a json object
+// takes request, response, status code and object to send
 const respondJSON = (request, response, status, object) => {
   response.writeHead(status, { 'Content-Type': 'application/json' });
   response.write(JSON.stringify(object));
   response.end();
 };
 
-//function to respond without json body
-//takes request, response and status code
+// function to respond without json body
+// takes request, response and status code
 const respondJSONMeta = (request, response, status) => {
   response.writeHead(status, { 'Content-Type': 'application/json' });
   response.end();
 };
 
-// success response with default message
-const success = (request, response, message = 'This is a successful response.') => {
-  // message to send
-  const responseJSON = {
-    message,
-  };
-  // send our json with a success status code
-  return respondJSON(request, response, 200, responseJSON);
-};
-
-// error response
-const error = (request, response, message, id, statusCode) => {
-  // error message with a description and consistent error id
-  const responseJSON = {
-    message,
-    id,
-  };
-
-  // return our json with a error code
-  return respondJSON(request, response, statusCode, responseJSON);
-};
-
 // function to show not found error
 const notFound = (request, response) => {
-	if(request.method === 'HEAD'){
-		return respondJSONMeta(request, response, 404);
-	}
-	const responseJSON = {
-		message: 'Name and age are both required.',
-		id: 'notFound',
-	};
-	
-	respondJSON(request, response, 404, responseJSON);
+  if (request.method === 'HEAD') {
+    return respondJSONMeta(request, response, 404);
+  }
+  const responseJSON = {
+    message: 'Name and age are both required.',
+    id: 'notFound',
+  };
+
+  return respondJSON(request, response, 404, responseJSON);
 };
 
 const getUsers = (request, response) => {
-	if(request.method === 'HEAD'){
-		return respondJSONMeta(request, response, 200);
-	}
-	const responseJSON = {
-    users
+  if (request.method === 'HEAD') {
+    return respondJSONMeta(request, response, 200);
+  }
+  const responseJSON = {
+    users,
   };
-	return respondJSON(request, response, 200, responseJSON);
-}
+  return respondJSON(request, response, 200, responseJSON);
+};
 
-//function to add a user from a POST body
+// function to add a user from a POST body
 const addUser = (request, response, body) => {
-  //default json message
+  // default json message
   const responseJSON = {
     message: 'Name and age are both required.',
   };
 
-  //check to make sure we have both fields
-  //We might want more validation than just checking if they exist
-  //This could easily be abused with invalid types (such as booleans, numbers, etc)
-  //If either are missing, send back an error message as a 400 badRequest
+  // check to make sure we have both fields
+  // We might want more validation than just checking if they exist
+  // This could easily be abused with invalid types (such as booleans, numbers, etc)
+  // If either are missing, send back an error message as a 400 badRequest
   if (!body.name || !body.age) {
     responseJSON.id = 'missingParams';
     return respondJSON(request, response, 400, responseJSON);
   }
 
-  //default status code to 201 created
+  // default status code to 201 created
   let responseCode = 201;
 
-  //if that user's name already exists in our object
-  //then switch to a 204 updated status
+  // if that user's name already exists in our object
+  // then switch to a 204 updated status
   if (users[body.name]) {
     responseCode = 204;
   } else {
-    //otherwise create an object with that name
+    // otherwise create an object with that name
     users[body.name] = {};
   }
 
-  //add or update fields for this user name
+  // add or update fields for this user name
   users[body.name].name = body.name;
   users[body.name].age = body.age;
 
-  //if response is created, then set our created message
-  //and sent response with a message
+  // if response is created, then set our created message
+  // and sent response with a message
   if (responseCode === 201) {
     responseJSON.message = 'Created Successfully';
     return respondJSON(request, response, responseCode, responseJSON);
@@ -112,7 +90,7 @@ const addUser = (request, response, body) => {
 // In this syntax, you can do getCats:getCats, but if they
 // are the same name, you can short handle to just getCats,
 module.exports = {
-	getUsers,
-	notFound,
-	addUser,
+  getUsers,
+  notFound,
+  addUser,
 };
--- src/server.js
+++ src/server.js
@@ -15,46 +15,46 @@ const urlStruct = {
   '/': htmlHandler.getIndex,
   '/style.css': htmlHandler.getCss,
   '/favicon.ico': htmlHandler.getFavicon,
-	'/getUsers': jsonHandler.getUsers,
-	'/addUser': jsonHandler.addUser,
+  '/getUsers': jsonHandler.getUsers,
+  '/addUser': jsonHandler.addUser,
   notFound: jsonHandler.notFound,
 };
 
-//handle POST requests
+// handle POST requests
 const handlePost = (request, response, parsedUrl) => {
-  //if post is to /addUser (our only POST url)
+  // if post is to /addUser (our only POST url)
   if (parsedUrl.pathname === '/addUser') {
     const res = response;
 
-    //uploads come in as a byte stream that we need 
-    //to reassemble once it's all arrived
+    // uploads come in as a byte stream that we need
+    // to reassemble once it's all arrived
     const body = [];
 
-    //if the upload stream errors out, just throw a
-    //a bad request and send it back 
-    request.on('error', (err) => {
-      console.dir(err);
+    // if the upload stream errors out, just throw a
+    // a bad request and send it back
+    request.on('error', (/* err */) => {
+      // console.dir(err);
       res.statusCode = 400;
       res.end();
     });
 
-    //on 'data' is for each byte of data that comes in
-    //from the upload. We will add it to our byte array.
+    // on 'data' is for each byte of data that comes in
+    // from the upload. We will add it to our byte array.
     request.on('data', (chunk) => {
-      body.push(chunk); 
+      body.push(chunk);
     });
 
-    //on end of upload stream. 
+    // on end of upload stream.
     request.on('end', () => {
-      //combine our byte array (using Buffer.concat)
-      //and convert it to a string value (in this instance)
+      // combine our byte array (using Buffer.concat)
+      // and convert it to a string value (in this instance)
       const bodyString = Buffer.concat(body).toString();
-      //since we are getting x-www-form-urlencoded data
-      //the format will be the same as querystrings
-      //Parse the string into an object by field name
+      // since we are getting x-www-form-urlencoded data
+      // the format will be the same as querystrings
+      // Parse the string into an object by field name
       const bodyParams = query.parse(bodyString);
 
-      //pass to our addUser function
+      // pass to our addUser function
       jsonHandler.addUser(request, res, bodyParams);
     });
   }
@@ -71,15 +71,13 @@ const onRequest = (request, response) => {
   // for the sake of this example
   if (request.method === 'POST') {
     handlePost(request, response, parsedUrl);
-  } else {
-    // check if the path name (the /name part of the url) matches
-    // any in our url object. If so call that function. If not, default to notFound.
-    if (urlStruct[parsedUrl.pathname]) {
-      urlStruct[parsedUrl.pathname](request, response);
-    } else {
-      urlStruct.notFound(request, response);
-    }
   }
+
+  if (urlStruct[parsedUrl.pathname]) {
+    urlStruct[parsedUrl.pathname](request, response);
+  }
+
+  urlStruct.notFound(request, response);
 };
 
 // start HTTP server
